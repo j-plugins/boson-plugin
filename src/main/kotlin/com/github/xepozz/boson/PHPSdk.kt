f48393bfd353b51db26cb0ff12f9a33f311850b9
Dmitriy Derepko <xepozz@list.ru>
2025-08-24 21:07:21 +0400

    feat: add web components classes

--- src/main/kotlin/com/github/xepozz/boson/PHPSdk.kt
+++ src/main/kotlin/com/github/xepozz/boson/PHPSdk.kt
@@ -2,4 +2,6 @@ package com.github.xepozz.boson
 
 object PHPSdk {
     const val WEB_COMPONENT = "\\Boson\\WebView\\Api\\WebComponents\\WebComponent"
+    const val WEB_COMPONENT_API_INTERFACE = "\\Boson\\WebView\\Api\\WebComponentsApiInterface"
+    const val WEB_VIEW_WEB_COMPONENTS = "\\Boson\\WebView\\Api\\WebComponents\\WebViewWebComponents"
 }
